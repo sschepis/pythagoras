36b4a460dc385022faa1889ce492f8d5a03f042e
CoderBoo <sschepis@gmail.com>
2014-05-03 23:01:46 -0700

    .

--- app/oscillator.js
+++ app/oscillator.js
@@ -3,8 +3,22 @@ var oscillators = {};
 
 var getOscillator = function(freq, play) {
     var oscillator = context.createOscillator();
-    oscillator.type = 0;
+    //oscillator.type = 0;
+
+    var real = new Float32Array(4096);
+    var imag = new Float32Array(4096);
+
+    var a1 = 0.0;
+    var b1 = 1.0;
+
+    var shift = 2 * Math.PI * 0.5; // Shift the waveform 50%
+    real[1] = a1 * Math.cos(shift) - b1 * Math.sin(shift);
+    imag[1] = a1 * Math.sin(shift) + b1 * Math.cos(shift);
+    var wt = context.createWaveTable(real, imag);
+
     oscillator.frequency.value = freq;
+    oscillator.setWaveTable(wt);
+    
     if(play) {
         oscillator.connect(context.destination);
         oscillator.noteOn && oscillator.noteOn(0);
