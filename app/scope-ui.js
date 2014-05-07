a787b9dc854959edf66301cd465994e0b4c78b75
Sebastian Schepis <sschepis@gmail.com>
2014-05-06 23:55:21 -0700

    .

---
+++ app/scope-ui.js
@@ -0,0 +1,44 @@
+
+// shim layer with setTimeout fallback
+window.requestAnimationFrame = window.requestAnimationFrame       ||
+  window.webkitRequestAnimationFrame;
+
+function pushme() {
+  pwmOsc.stop(0);
+  //window.cancelAnimationFrame(rafID);
+}
+var rafID;
+var myOscilloscope = null;
+
+function draw() {  
+  if (myOscilloscope)
+    myOscilloscope.draw(myCanvas.myContext);
+
+  rafID = requestAnimationFrame( draw );
+}
+
+function setupCanvas( container ) {
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = 512; 
+  canvas.height = 512; 
+  canvas.myContext = canvas.getContext( '2d' );
+
+  if (container)
+    container.appendChild( canvas );
+  else
+    document.body.appendChild( canvas );
+  return canvas;
+}
+
+function init(){
+  myCanvas = setupCanvas();
+  setupAudio( myCanvas );
+
+  draw.bind(myCanvas)();
+}
+
+window.addEventListener("load", init );
+
+function dutycyclechange() {
+  pwmOsc.setDutyCycle(1-parseFloat(document.getElementById("dutycycle").value));
+}
